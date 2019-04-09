09c1ea7304a0f96cbb67bc2200a30121abdf0a58
THANAWAT-LEGACY <49397703+THANAWAT-LEGACY@users.noreply.github.com>
2019-04-09 15:12:32 +0700

    test Commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Test09 Edit <code>src/App.js</code> and save to reload.
+            Test Commit Edit <code>src/App.js</code> and save to reload.
           </p>
           <a
             className="App-link"
