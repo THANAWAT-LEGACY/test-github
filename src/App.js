efc049b4fff4a01cde0802a394d5f0f2a504645e
THANAWAT KHAMCHANA (Nun) <49397703+THANAWAT-LEGACY@users.noreply.github.com>
2020-02-20 16:51:37 +0700

    Revert "Test V3"

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Project V.3</h1>
+        <h1>Project V.2</h1>
         <div>
           <h2>Dev-01</h2>
           <p>Detail 1  aaaaaaaaaaaaaaaaaa</p>
