a3c4b6f349bf0d237056cbbb48177122ccc0051e
THANAWAT-LEGACY <49397703+THANAWAT-LEGACY@users.noreply.github.com>
2020-03-29 12:54:08 +0700

    Edit Test V1.2

--- src/pages/work-all.js
+++ src/pages/work-all.js
@@ -5,17 +5,15 @@ export class WorkAll extends Component {
     render() {
         return (
             <>
-            <h2>Edit Test</h2>
-            <Row>
-                <Col>
-                   All 1  V1.
-
-
+                <h2>Edit Test V2</h2>
+                <Row>
+                    <Col>
+                        All 1  V1.   ทดสอบ v2
                 </Col>
-                <Col></Col>
-            </Row>
-            <div>
-              Add All Dev 2
+                    <Col></Col>
+                </Row>
+                <div>
+                    Add All Dev 2   ทดสอบ v2
             </div>
             </>
         )
