8109dcc5bbc2ef0185e347f8fb17d302fa2a93bb
THANAWAT-LEGACY <49397703+THANAWAT-LEGACY@users.noreply.github.com>
2020-03-29 12:59:07 +0700

    Revert "Project V. 1.03.29.02 #20"
    This reverts commit 87b160aab6e33b8c1adc0bb52f95e4c87d5636ec, reversing
    changes made to c78c05147b24a833a386c5a86e166bf68b21ca66.

--- src/pages/work-all.js
+++ src/pages/work-all.js
@@ -5,15 +5,17 @@ export class WorkAll extends Component {
     render() {
         return (
             <>
-                <h2>Edit Test V2</h2>
-                <Row>
-                    <Col>
-                        All 1  V1.   ทดสอบ v2
+            <h2>Edit Test</h2>
+            <Row>
+                <Col>
+                   All 1  V1.
+
+
                 </Col>
-                    <Col></Col>
-                </Row>
-                <div>
-                    Add All Dev 2   ทดสอบ v2
+                <Col></Col>
+            </Row>
+            <div>
+              Add All Dev 2
             </div>
             </>
         )
