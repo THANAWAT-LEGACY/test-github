4c0461a4babdc8fad62ebefa3d5a0abe5119ef4b
THANAWAT-LEGACY <49397703+THANAWAT-LEGACY@users.noreply.github.com>
2020-03-29 11:53:18 +0700

    add v1

--- src/pages/work-all.js
+++ src/pages/work-all.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
+import { Row, Col } from 'react-bootstrap'
 
 export class WorkAll extends Component {
     render() {
         return (
-            <div>
-                
-            </div>
+            <Row>
+                <Col>
+                   All 1  V1.
+
+
+                </Col>
+                <Col></Col>
+            </Row>
         )
     }
 }
